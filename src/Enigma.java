37b5d0a65eb470113866ac70000da01c0047fe26
yoliner <yoline@172.17.71.177>
2016-11-10 14:56:11 +0100

    Ajout des deux classes

---
+++ src/Enigma.java
@@ -0,0 +1,56 @@
+
+public class Enigma {
+
+	//Attributes
+	
+	private EnigmaItem item;
+	private String answer;
+	private String question;
+	private Item reward;
+	private boolean isResolved;
+	private boolean itemWaited;  //Return true if an item is waited for the answer
+
+	//Methods
+		
+	//Constructor
+	Enigma (String quest) {
+		
+	}
+	
+	//Set the answer when it is just a text 
+	public void setAnswer (String reponse){
+		
+	}
+	
+	//Set the answer whant it is an object
+	public void setAnswer (Objet item){
+		
+	}
+	
+	//Set the answer when it is an information
+	public void setAnswer (Information item){
+		
+	}
+	
+	//Return true if enigma is resolved
+	public boolean resolveEnigma (String answer){
+		return false; 
+	}
+	
+	//Return true if enigma is resolved
+	public boolean resolveEnigma (EnigmaItem item){
+		return false;
+	}
+	
+	//Give a reward if the enigma is resolved
+	public Item rewarded(){
+		
+	}
+	
+	//Return true if enigma is already resolved and false if not
+	public boolean alreadyResolved(){
+		return false;
+	}
+	
+	
+}
