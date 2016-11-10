7bd8e1dc13db73f6abd2a4809030a6e6ac9e2e88
yoliner <yoline@172.17.71.177>
2016-11-10 15:02:15 +0100

    Modification de la classe Information

--- src/Information.java
+++ src/Information.java
@@ -1,4 +1,26 @@
 
 public class Information {
 
+	//Attributes
+	private String description
+	
+	//Methods
+	
+	//Constructor
+	Information (String descriptionInfo){
+		
+	}
+	
+	
+	//Return the description about the item
+	public String getDescription(){
+		
+	}
+	
+	
+	//Diplay the information
+	public void displayInformation(){
+		
+	}
+	
 }
