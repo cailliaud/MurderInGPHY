31e5cd424f1bd139bf67fabf66e562698d2120a3
Clarisse <Clarisse@172.17.70.175>
2016-11-10 16:35:18 +0100

    door correct

---
+++ src/Core/Door.java
@@ -0,0 +1,23 @@
+import src.Room;
+
+
+
+public class Door {
+
+	private boolean open;
+	private Room room1;
+	private Room room2;
+	
+	public Door (boolean openDoor, Room roomDoor1, Room roomDoor2){
+		
+	}
+	
+	public boolean getOpen(){
+		return false;
+	}
+	
+	public void setOpen(){
+		
+	}
+
+}
