88d52c2792b82778f6707fea0ee3850a41468b57
conte <conte@172.17.72.91>
2016-11-10 14:57:41 +0100

    Ajout des classes player PNJ et personage

---
+++ src/PNJ.java
@@ -0,0 +1,15 @@
+
+public class PNJ extends Personage{
+
+	private Enigma enigma;
+	
+	public void addEnigma(Enigma enigma){
+		
+	}
+	public void resolveEnigma(String answer){
+		
+	}
+	public void resolveEnigma(EnigmaItem item){
+		
+	}
+}
--- src/Personage.java
+++ src/Personage.java
@@ -1,4 +1,23 @@
 
 public abstract class Personage {
 
+	private String name;
+	private Room current Room;
+	private Boolean isKiller;
+	
+	public void Personage(String name, Room currentRoom, Boolean isKiller){
+	}
+	
+	public String getName(){
+		
+	}
+	public Boolean isKiller(){
+		
+	}
+	public String getRoom(){
+		
+	}
+	public void setCurrentRoom(Room newRoom){
+	}
+	
 }
