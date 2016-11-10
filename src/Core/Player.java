41b233739e6d8d8cb8afc72ef6b1af68e16d3743
conte <conte@172.17.72.91>
2016-11-10 18:01:01 +0100

    player

--- src/Core/Player.java
+++ src/Core/Player.java
@@ -2,6 +2,7 @@ package Core;
 public class Player extends Personage{
 
 	private Owned inventory;
+	
 
 	public Player(String name, Room currentRoom, Boolean isKiller, Owned inventory){
 		super( name,  currentRoom,  isKiller);
@@ -11,12 +12,13 @@ public class Player extends Personage{
 	//allows player to move
 	public void move(){
 	}
-
-	//player can see his inventaire
+	
+	//player can see his inventary
 	public void seeInventaire(){
 	}
-
+	
 	//gives the name of the killer
-	public void giveKiller( String name){
+	public void giveKiller(){
 	}
+	
 }
