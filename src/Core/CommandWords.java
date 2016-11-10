01c55a33e123e093f2605d291fad79ca28b263ac
cailliaud <cailliaud.a@gmail.com>
2016-11-10 15:43:08 +0100

    Ajout Bolt et change command , items et personage

--- src/Core/CommandWords.java
+++ src/Core/CommandWords.java
@@ -2,27 +2,16 @@ package Core;
 
 public class CommandWords {
 
-	/**
-	 * Constructor - initialize the command words
-	 */
+
 	public CommandWords()
 	{
 		// to complete
 	}
 	
-	/**
-	 * Check whether a given String is a valid command word.
-	 * @return true if a given string is a valid command,
-	 * false if it isn't.
-	 */
+
 	public boolean isCommand(String aString)
 	{
-		for(int i = 0; i < validCommands.length; i++) {
-			if(validCommands[i].equals(aString))
-				return true;
-		}
-		// if we get there, the string has not been found in the commands
-		return false;
+
 		
 	}
 }
