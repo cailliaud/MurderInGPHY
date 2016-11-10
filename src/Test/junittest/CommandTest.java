dba11f17f6655fe966f6fb2afd0be1dfc2352e6b
Zibbulon <steve.fiolleau@outlook.fr>
2016-11-10 15:32:12 +0100

    test d'un commit d'un junit test

---
+++ src/Test/junittest/CommandTest.java
@@ -0,0 +1,34 @@
+package Test.junittest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CommandTest {
+
+	@Test
+	public void testCommand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCommandWord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSecondWord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsUnknown() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasSecondWord() {
+		fail("Not yet implemented");
+	}
+
+}
