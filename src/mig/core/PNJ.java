23af5cca915864cf564cf3470181aeaa7493c04a
conte <conte@172.17.69.93>
2016-11-22 10:17:15 +0100

    PNJ et Personage commentaires

--- src/mig/core/PNJ.java
+++ src/mig/core/PNJ.java
@@ -1,15 +1,51 @@
 package mig.core;
 
+/**
+ * <b>PNJ is the class representing PNJ present in the game. </b>
+ * <p>
+ * These class is in inheritence with the class Personage. 
+ * PNJ is a personage non-player. He is here to help the player, for example to give him clue or item after an enigma, to help to discover the killer. 
+ * 
+ * </p>
+ * <p>
+ * PNJ contains these elements :
+ * </p>
+ * <ul>
+ * <li>A name, fixed when the game is created. In inheritence with personage.</li>
+ * <li>Is the personage is the killer</li>
+ * <li>Is the PNJ is use in a room.</li>
+ * <li>Have an enigma for the player.</li>
+ * <li>Have clue or item for the player after enigma</li>
+ * </li>
+ * </ul>
+ * 
+ * @see Personage
+ * @see Enigma 
+ * 
+ * @author Group8
+ * @version 22/11/2016
+ */
+
 public class PNJ extends Personage{
 
+	/** 
+	 * a PNJ has an enigma to ask to the player.
+	 */
 	private Enigma enigma;
 	/**
-	 * It represents if the personage is alive or not.
+	 * It represents if the personage is alive or not. If he is already use in a room. A PNJ can't be used in two different rooms.
 	 */
 	private boolean alive;
 	
 	
 	//constructor
+	/**
+	 *Constructor of the PNJ. Allows to create the PNJ.
+	 *
+	 * @param name The name has to be in lower case for the security. So that there is no matter when we enter a name with upper case.
+	 * @param isKiller Is a boolean fixed in the beginning if the personage is the killer.
+	 * @param enigma Is the enigma of the PNJ for the player.
+	 */
 	public PNJ(String name,  Boolean isKiller, Enigma enigma){	
 		this.name=name;
 		this.isKiller=isKiller;
@@ -17,16 +53,28 @@ public class PNJ extends Personage{
 	}
 	
 	//is the enigma of this PNJ
+	/**
+	 * Allows to add an enigma for a PNJ
+	 * 
+	 * @param enigma Is the enigma of the PNJ
+	 */
 	public void addEnigma(Enigma enigma){
 	}
 	
 	//PNJ gives a clue to the player
+	/**
+	 * PNJ gives a clue to the player. If the answer is correct, he gives a clue to the player.
+	 * @param answer Is the answer of the enigma
+	 */
 	public void resolveEnigma(String answer){	
 	}
 	
 	//PNJ gives an item the the player
-	public void resolveEnigma(EnigmaItem item){
-		
+	/**
+	 * PNJ gives an item the the player, after the correct answer.
+	 * @param item Is the item given to the player.
+	 */
+	public void resolveEnigma(EnigmaItem item){	
 	}
 	
 	/**
--- src/mig/core/Personage.java
+++ src/mig/core/Personage.java
@@ -1,20 +1,56 @@
 package mig.core;
 
+/**
+ * <b>Personage is the class representing the personages in the game. It is a inheritress class for PNJ and Player class.</b>
+ * <p>
+ * These class is an abstract class because the user is forbidden to create a player or a PNJ. 
+ * There is no function setName because it can't be modify. 
+ * </p>
+ * <p>
+ * Personage contains these elements :
+ * </p>
+ * <ul>
+ * <li>A name, fixed when the game is created</li>
+ * <li>Is the personage is the killer</li>
+ * </li>
+ * </ul>
+ * 
+ * @author Group8
+ * @version 22/11/2016
+ */
+
 public abstract class Personage {
 
+	/**
+	 * It is the name of the personage.
+	 * It is set in the constructor and cannot be changed after. 
+	 */
 	protected String name;
+	
+	/**
+	 * It says if the personage is the killer.
+	 * It is set in the constructor and cannot be changed after 
+	 */
 	protected boolean isKiller;
 	
 
-	
-
-	
 	//return the name of the personage
+	/**
+	 * 
+	 * @param name is the name of the personage
+	 * @return the name of the personage
+	 */
 	public   String getName(String name){
 		return null;
 	}
 
+	
 	//return the boolean if the character is the killer
+	/**
+	 * 
+	 * @param isKiller is a boolean fixed in the beginning if the personage is the killer.
+	 * @return  false if the personage is not the killer.
+	 */
 	public Boolean getIsKiller(Boolean isKiller){
 		return false;
 	}
