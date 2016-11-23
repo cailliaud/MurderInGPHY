859097a1b15247d37362b9686551d0324e733315
yoliner <yoline@172.17.71.177>
2016-11-23 10:12:21 +0100

    Class test enigma

---
+++ test/test/core/EnigmaTest.java
@@ -0,0 +1,93 @@
+package test.core;
+
+import static org.junit.Assert.*;
+import mig.core.Enigma;
+import mig.core.Information;
+import mig.core.PhysicalObject;
+import mig.core.Item;
+
+import org.junit.Test;
+
+/**
+ * JUnit test for the class Enigma
+ * @see Enigma
+ * @author group8
+ * @version 23/11/16
+ *
+ */
+
+public class EnigmaTest {
+	private PhysicalObject object;
+	private Information info;
+	private Item item;
+	private Item item2; 
+	private boolean isResolved = false;
+
+	/** 
+	 * Default constructor for test class Enigma
+	 */
+	public void testEnigma() {	
+	}
+
+	/**
+	 * Method testSetAnswerString
+	 * <p>Checks if the response given as parameter in a String </p>
+	 */
+	@Test
+	public void testSetAnswerString() {
+		info= new Information("Answer","My response is blabla");
+		assertEquals("Answers", info.getName()); 
+	}
+
+	/**
+	 * Method testSetAnswerEnigmaItem
+	 * <p> Checks if the response given as parameter is an EnigmaItem </p>
+	 */
+	@Test
+	public void testSetAnswerEnigmaItem() {
+		object= new PhysicalObject ("Book", "For ADA code");
+		assertEquals("Book", object.getName()); 
+	}
+
+	/**
+	 * Method testResolveEnigmaString
+	 * <p> Return false if the Enigma is not resolved if the response is a String </p>
+	 */
+	@Test
+	public void testResolveEnigmaString() {
+		info= new Information ("Answer", "My response is toto");
+		assertEquals (false, info.getExist());
+	}
+
+	/**
+	 * Method testResolveEnigmaItem
+	 * <p> Return false if the Enigma is not resolved if the response is an PhysicalObject </p> 
+	 */
+	@Test
+	public void testResolveEnigmaEnigmaItem() {
+		object = new PhysicalObject("Book", "For Java code");
+		assertEquals (false, object.getExist());
+	}
+
+	/**
+	 * Method testRewarded
+	 * <p> Return null when the Enigma is not resolved </p>
+	 */
+	@Test
+	public void testRewarded() {
+		item = new Information("Next room", "To find murder");
+		item2 = new PhysicalObject ("Key", "For open door");
+		assertEquals (null, item.getName());
+		assertEquals (null, item2.getName());
+	}
+
+	/**
+	 * Method testAlreadyResolved
+	 * <p> Return false if the Enigma is not resolved </p> 
+	 */
+	@Test
+	public void testAlreadyResolved() {
+		assertEquals (false, isResolved.getExist());
+	}
+
+}
