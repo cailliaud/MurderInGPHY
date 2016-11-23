50eb39267222728f37cf08cc56dc8fab2fe6d815
FP Légeron <FP Lgeron@192.168.24.138>
2016-11-23 09:49:47 +0100

    Booya !

---
+++ test/test/core/KeyTest.java
@@ -0,0 +1,31 @@
+package test.core;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import mig.core.Key;
+
+
+public class KeyTest {
+
+	public KeyTest() {
+	}
+	
+@Before
+public void setUp()
+{
+	new Key("");
+}
+
+@After
+public void tearDown()
+{
+}
+
+@Test
+public void testName() {
+}
+
+public void Key() {
+}
+}
