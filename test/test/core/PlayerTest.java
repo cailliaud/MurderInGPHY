38c71b0bf2a3eacaffd48b4ddc7edbbb68da59c0
yoliner <yoline@172.17.71.177>
2016-11-24 12:27:39 +0100

    Class test Player

---
+++ test/test/core/PlayerTest.java
@@ -0,0 +1,49 @@
+package test.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import mig.core.Personage;
+import mig.core.Room;
+
+
+public class PlayerTest {
+	
+	private String name ;
+	private Room currentRoom;
+	
+	public PlayerTest (){
+		name = new String ("Toto");
+		currentRoom = new Room ("TP1");
+	}
+	
+	/**
+	 * Default constructor for test class Player
+	 */
+
+	@Test
+	public void testPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSeeInventaire() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveItem() {
+		fail("Not yet implemented");
+	}
+
+}
