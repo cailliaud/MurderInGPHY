47374d411258c25291439b16d7cbd827854f3e0a
FP Légeron <FP Lgeron@172.18.17.42>
2016-11-24 11:10:31 +0100

    # WARNING: head commit changed in the meantime
    Booya !
    
    Signed-off-by: FP Légeron <FP Légeron@172.18.17.42>

--- test/test/core/KeyTest.java
+++ test/test/core/KeyTest.java
@@ -1,31 +1,65 @@
 package test.core;
 
+import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import mig.core.Key;
 
+/**
+ * The test class KeyTest.
+ * Unit tests of the Key Class.
+ * 
+ * @author FP Légeron
+ * @version 24/11/2016
+ */
 
-public class KeyTest {
-
-	public KeyTest() {
-	}
+public class KeyTest
+{
+	private Key myKey;
 	
+/**
+ * Default constructor for test class KeyTest.
+ */
+	public KeyTest()
+	{
+	}
+		
+/**
+ * Sets up the test fixture.
+ * Called before every test case method.
+ * Initialize one key for each test, whose name is CléForgée.
+ */
 @Before
 public void setUp()
 {
-	new Key("");
+	myKey = new Key("CléForgée");
 }
 
+/**
+ * Tears down the test fixture.
+ * Called after every test case method. Nothing to do in this case.
+ */
 @After
 public void tearDown()
 {
 }
 
+/**
+ * The method assertEquals asserts that the name of the object is correctly considered as an attribute.
+ */
 @Test
-public void testName() {
+public void testNameCléForgée() {
+	assertEquals("CléForgée", myKey.getName());
 }
 
-public void Key() {
+/**
+ * The method assertNotNull asserts an object isn't null.
+ */
+@Test
+public void Assert() {
+	assertNotNull("CléForgée", myKey);
 }
 }
+
+
