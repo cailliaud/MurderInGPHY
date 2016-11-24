0db474741bceb8e1b62a31d90dafcca52e5a4316
FP Légeron <FP Lgeron@172.18.17.42>
2016-11-24 12:33:03 +0100

    KeyTest Final

--- test/test/core/KeyTest.java
+++ test/test/core/KeyTest.java
@@ -10,57 +10,65 @@ import mig.core.Key;
  * The test class KeyTest.
  * Unit tests of the Key Class.
  * 
- * @author FP Légeron
+ * @author Group8
  * @version 24/11/2016
  */
 
 public class KeyTest
 {
 	private Key myKey;
-	
-/**
- * Default constructor for test class KeyTest.
- */
+
+	/**
+	 * Default constructor for test class KeyTest.
+	 */
 	public KeyTest()
 	{
 	}
-		
-/**
- * Sets up the test fixture.
- * Called before every test case method.
- * Initialize one key for each test, whose name is CléForgée.
- */
-@Before
-public void setUp()
-{
-	myKey = new Key("CléForgée");
+
+	/**
+	 * Sets up the test fixture.
+	 * Called before every test case method.
+	 * Initialize one key for each test, whose name is CléForgée.
+	 */
+	@Before
+	public void setUp()
+	{
+		myKey = new Key("CléForgée");
+	}
+
+	/**
+	 * Tears down the test fixture.
+	 * Called after every test case method. Nothing to do in this case.
+	 */
+	@After
+	public void tearDown()
+	{
+	}
+
+	/**
+	 * The method assertEquals asserts that the name of the object is correctly considered as an attribute.
+	 */
+	@Test
+	public void testNameCléForgée() {
+		assertEquals("CléForgée", myKey.getName());
+	}
+
+	/**
+	 * The method assertNotNull asserts an object isn't null.
+	 */
+	@Test
+	public void TestobjetisNotNull() {
+
+		assertNotNull("CléForgée", myKey);
+	}
+
+	/**
+	 * The method _ _ _ asserts a key can't exist without a name.
+	 */
+	@Test
+	public void TestObject() {
+		Key myKey= new Key("");
+		assertNotEquals("", myKey.getName());
+
+	}
 }
-
-/**
- * Tears down the test fixture.
- * Called after every test case method. Nothing to do in this case.
- */
-@After
-public void tearDown()
-{
-}
-
-/**
- * The method assertEquals asserts that the name of the object is correctly considered as an attribute.
- */
-@Test
-public void testNameCléForgée() {
-	assertEquals("CléForgée", myKey.getName());
-}
-
-/**
- * The method assertNotNull asserts an object isn't null.
- */
-@Test
-public void Assert() {
-	assertNotNull("CléForgée", myKey);
-}
-
-}
-
-
