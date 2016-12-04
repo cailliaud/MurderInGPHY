5c90deebbef4c858397f0961f3fcc7275aff3b1a
Constance_2 <Constance_2@pc-constance.home>
2016-12-04 14:34:05 +0100

    création du bouton dénoncer

--- src/mig/view/DenounceButton.java
+++ src/mig/view/DenounceButton.java
@@ -1,29 +1,46 @@
 package mig.view;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
+
 import mig.core.Game;
 import mig.exceptions.GameOver;
 
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
+
+
 
 public class DenounceButton extends JButton {
 	private Game game;
 	public DenounceButton(Game game){
+		this.game= game;
+		JLabel message = new JLabel("<html>Who is the killer ?? <p>BE PRECISE in writing the name to win!</p> ");
+		JLabel message1 = new JLabel("<html>Good job, you found the murderer!! <p>"  + game.getKiller().getName()+" is in prison !!!</p>"); 
+		JLabel message2 = new JLabel("GAME OVER !!");
+		message.setFont(new Font("Calibri",Font.BOLD+Font.ITALIC,25));
+		message1.setFont(new Font("Calibri",Font.BOLD,25));
+		message2.setFont(new Font("Calibri",Font.BOLD,25));
 		this.setText("Denounce");
 		this.game=game;
 		this.addActionListener(
 				ae -> {
 					JOptionPane den = new JOptionPane(), den2 = new JOptionPane();
-					String rep = den.showInputDialog(null, "Who is the killer ?? ", "Denounce", JOptionPane.QUESTION_MESSAGE);
+					ImageIcon img2 = new ImageIcon("resources/game_over.png");
+					ImageIcon img1 = new ImageIcon("resources/good_job.png");
+					String rep = den.showInputDialog(null, message, "Denounce",JOptionPane.QUESTION_MESSAGE);
 					try {
 						game.giveKiller(rep);
-						den2.showMessageDialog(null, "GOOD JOB !!", "You win" , JOptionPane.INFORMATION_MESSAGE);
+						den2.showMessageDialog(null, message1 , "You won" , JOptionPane.INFORMATION_MESSAGE, img1);
+						
 					} catch (GameOver e) {
 						// TODO Auto-generated catch block
-						den2.showMessageDialog(null, "GAME OVER !!! ", "Sorry" , JOptionPane.INFORMATION_MESSAGE);
-						
-					}
+						den2.showMessageDialog(null, message2, "Sorry" , JOptionPane.INFORMATION_MESSAGE, img2);
 					
+					}		
+					System.exit(0);
 				}
 				);
 
