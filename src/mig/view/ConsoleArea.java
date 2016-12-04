78cbdf84b2879e08360daeabb8d47d142be8f470
cailliaud <cailliaud.a@gmail.com>
2016-12-04 16:41:17 +0100

    Quelques petites modifs de console area

--- src/mig/view/ConsoleArea.java
+++ src/mig/view/ConsoleArea.java
@@ -1,14 +1,18 @@
 package mig.view;
 
+import java.awt.Color;
+
+import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
 import mig.core.Game;
 
-public class ConsoleArea extends JTextArea {
+public class ConsoleArea extends JLabel {
 	private Game game;
 	public ConsoleArea(Game game){
 		this.game = game;
 		this.setText("Bienvenue dans Murder in GPHY " + game.myPlayer.getName() );
+		this.setForeground (Color.white);
 		
 	}
 
